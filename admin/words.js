1f83613bc0cf24181cc36f2094cc04b4a366d36d
swissglider <npm@mailschweiz.com>
2020-10-28 08:55:27 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'harmonize-battery-states adapter settings': {
+        en: 'Adapter settings for harmonize-battery-states',
+        de: 'Adaptereinstellungen für harmonize-battery-states',
+        ru: 'Настройки адаптера для harmonize-battery-states',
+        pt: 'Configurações do adaptador para harmonize-battery-states',
+        nl: 'Adapterinstellingen voor harmonize-battery-states',
+        fr: "Paramètres d'adaptateur pour harmonize-battery-states",
+        it: "Impostazioni dell'adattatore per harmonize-battery-states",
+        es: 'Ajustes del adaptador para harmonize-battery-states',
+        pl: 'Ustawienia adaptera dla harmonize-battery-states',
+        'zh-cn': 'harmonize-battery-states的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'option2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        'zh-cn': '选项2',
+    },
+};
